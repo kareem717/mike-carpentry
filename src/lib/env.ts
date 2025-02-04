80506d0284a1b1a87c46e30b181bf2985b1da751
Kareem Yakubu <kareem.717@icloud.com>
2025-02-04 17:05:49 -0500

    feat: add seo and misc files

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -8,11 +8,7 @@ export const env = createEnv({
 			.default("development"),
 	},
 	client: {
-		NEXT_PUBLIC_APP_URL: z
-			.string()
-			.url()
-			.min(1)
-			.default("http://localhost:3000"),
+		NEXT_PUBLIC_APP_URL: z.string().url().min(1),
 	},
 	experimental__runtimeEnv: {
 		NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
